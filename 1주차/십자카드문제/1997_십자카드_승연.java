b7d9e5d73dcbb8cea21ec0aa71841b64aee4b258
yeonyeonKim <46700771+yeonyeonKim@users.noreply.github.com>
2020-07-12 21:44:38 +0900

    Update 1997_십자카드_승연.java

--- 1주차/십자카드문제/1997_십자카드_승연.java
+++ 1주차/십자카드문제/1997_십자카드_승연.java
@@ -9,9 +9,10 @@ class Main{
             arr[i] = sc.nextInt();
         }
         num = fun(arr[0]*1000+arr[1]*100+arr[2]*10+arr[3]);
+        //현재 받은수가 가진 수들로 만들 수 있는 최소 수인지 확인함
         for(int i=1111;i<=num;i++){
             if(fun(i)==i){
-                cnt++;
+                cnt++;//수가 최소 수이면 카운트
             }
         }
         System.out.println(cnt);
@@ -19,7 +20,7 @@ class Main{
     static int fun(int num){
         int min = num;
         for(int i=0;i<3;i++){
-            num = (num%1000)*10+(num/1000);
+            num = (num%1000)*10+(num/1000);//한자리씩 앞칸으로 이동하며 수를 만들어 확인
             if(num<min){
                 min = num;
             }
