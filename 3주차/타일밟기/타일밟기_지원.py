ac205e069a0b85823b10abe69c3df8fca243bd6d
Jiwon Cha <jc4va@virginia.edu>
2020-07-27 22:42:58 +0900

    week3

--- 3주차/타일밟기/타일밟기_지원.py
+++ 3주차/타일밟기/타일밟기_지원.py
@@ -2,14 +2,25 @@ import sys
 
 sys.stdin = open("input.txt", "r")
 n = int(input())
-tyles = list(map(int, input().split()))
-max_val = max(tyles)
-med_val = max_val // 2
-max_sum = 0
+tiles = list(map(int, input().split()))
+value = [-1] * 1000001
+dp = [[-1 for i in range(n)] for j in range(n)]
 
-for vals in range(med_val + 1):
-    for i in range(n):
-        val1 = tyles[i]
-        for j in range(i + 1, n):
-            val2 = tyles[j]
-            if
+result = 0
+
+for tile1 in range(n - 1, -1, -1):
+    num = tiles[tile1]
+    value[num] = tile1
+    for tile2 in range(tile1+1, n):
+        # 세번째 항: 2 * 두번째 수 - 첫번째 수
+        tile3 = 2 * tiles[tile2] - num
+        # 수 업데이트
+        if tile3 > 1000000 or value[tile3] == -1:
+            dp[tile1][tile2] = num + tiles[tile2]
+        # -1이 아닌 경우에
+        else:
+            dp[tile1][tile2] = num + dp[tile2][value[tile3]]
+            if result < dp[tile1][tile2]:
+                result = dp[tile1][tile2]
+
+print(result)
