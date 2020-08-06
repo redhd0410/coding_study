202d333187124a6577f93eb8628a58ae9d459b12
Jiwon Cha <jc4va@virginia.edu>
2020-08-06 20:54:29 +0900

    solve

--- 4주차/리조트/리조트_지원.py
+++ 4주차/리조트/리조트_지원.py
@@ -3,5 +3,8 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 N, M = map(int, input().split())
-dp = [0] * N + 1
+
+def solve(day, coupon):
+    if day > n:
+
 
--- 4주차/택배/택배_지원.py
+++ 4주차/택배/택배_지원.py
@@ -3,13 +3,39 @@ import sys, math
 sys.stdin = open("input.txt", "r")
 N, C = map(int, input().split())
 M = int(input())
-village = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+data = []
 
 for _ in range(M):
-    sender, receiver, box = map(int, input().split())
-    village[sender][receiver] = box
+    s, e, w = map(int, input().split())
+    data.append([s, e, w])
 
-# 0 인덱스 빼기
-village = [line[1:] for line in village]
+# 도착지 기준으로 오름차순 정렬
+data.sort(key=lambda x: (x[1], x[0]))
+rooms = [C] * N
 
+weight = 0
+for d in data:
+    print(d)
+    start = d[0]
+    end = d[1]
+    box = d[2]
+
+    # 자리 없을 때
+    if rooms[start] == 0:
+        continue
+
+    # 현재 용량이 박스보다 작은지 체크
+    # 용량이 작다면 남은 공간을 박스로 지정 (남은 공간 만큼만 옮김)
+    for i in range(start, end):
+        if rooms[i] < box:
+            print(box)
+            box = rooms[i]
+
+    # 마을 용량에서 박스 용량 빼줌 
+    for i in range(start, end):
+        rooms[i] -= box
+
+    weight += box
+
+print(weight)
